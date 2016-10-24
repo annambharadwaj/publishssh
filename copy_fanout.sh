fcea261ac2a558628eeb4cfa80b69106e7ea63ec
annambharadwaj <annambharadwaj@gmail.com>
2016-10-24 14:55:08 -0400

    Update copy_fanout.sh

--- copy_fanout.sh
+++ copy_fanout.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 pool=(10.152.0.51 10.152.0.52)
-$cmd="scp /var/lib/jenkins/jobs/Publish_over_SSH/workspace/jenkinsbuild.xml bannam@$pool:/tmp/builds"
+$cmd="scp bannam@192.168.110.15:/var/lib/jenkins/jobs/Publish_over_SSH/workspace/jenkinsbuild.xml bannam@$pool:/tmp/builds"
 /usr/bin/fanout --noping "$pool" "$cmd";
